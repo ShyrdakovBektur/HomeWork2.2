8c10389b957f0b32f4fed0fa296aebfb69be3d96
TM <shyrdakovbektur07@gmail.com>
2023-01-29 01:48:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        int k = 100;
+
+        int random_number1 = a + (int) (Math.random() * k);
+        System.out.println("1-ое случайное число: " + random_number1);
+
+        int random_number2 = a + (int) (Math.random() * k);
+        System.out.println("2-ое случайное число: " + random_number2);
+
+        int random_number3 = a + (int) (Math.random() * k);
+        System.out.println("3-е случайное число: " + random_number3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
